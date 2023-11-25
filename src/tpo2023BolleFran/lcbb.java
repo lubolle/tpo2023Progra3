15a4a2f7cd9f891a5ab2f4cd1378cbe862af2b35
lucas Bollella <lubolle@gmail.com>
2023-11-25 00:51:14 -0300

    se agrega lo ultimo

--- src/tpo2023BolleFran/lcbb.java
+++ src/tpo2023BolleFran/lcbb.java
@@ -5,25 +5,39 @@ import java.util.PriorityQueue;
 
 public class lcbb {
 
-	  private static int bestCost;
+	  private static int upper;
 	  private static int bestIndex = -1;
 
 	  private static class Node {
 
 	    private int index;
-	    private int cost;
-	    private int[] built;
-	    private int u;//optimista
-	    private int c;//pesimista
+	    private int nivel;
+//	    private int cost; // Costo de la matriz
+	    private int[] centros;//Este array es el que tiene la situacion de los centros
+	    private int U;//optimista
+	    private int C;//pesimista
 
-	    public Node(int index, int cost, int[] built,int u, int c) {
+	    //Constructor Node
+	    public Node(int index, int nivel, int[] centros,int U, int C) {
 	      this.index = index;
-	      this.cost = cost;
-	      this.built = built;
-	      this.u = u;
-	      this.c = c;
+	      this.nivel = nivel;
+//	      this.cost = cost;
+	      this.centros = centros;
+	      this.U = U;
+	      this.C = C;
 	    }
+//	    
+//	    public Node(int index, int cost, int buildCost, int U, int C) {
+//	        this.index = index;
+//	        this.cost = cost;
+//	        this.buildCost = buildCost;
+//	        this.U = U;
+//	        this.C = C;
+//	      }
 	  }
+	  
+	  
+	  
 	  public static int calcularU(int[][] M, int[] centros) {
 		  int c = 0;
 		  int flagCostosFijos = 0;// Bandera que uso para sumar solamente una vez los costos fijos
@@ -158,49 +172,80 @@ public class lcbb {
 		    return true;
 		}
 	  
-	  public static int branchAndBound(int[][] M, int[] built) {
-		// Inicializamos la variable bestCost.
-		bestCost = Integer.MAX_VALUE;
-	    // Inicializamos la variable bestIndex.
-	    bestIndex = -1;
-	    // Inicializamos la cola de prioridad con el nodo raíz.
-	    //La linea Comparator.comparingInt(n -> n.cost) basicamente es un comparador que compara los obejtos de tipo nodo por su costo.
-	    //En otras palabras, esta línea crea una cola de prioridad que siempre extraerá el nodo con el costo más bajo primero.
-	    
-	    PriorityQueue<Node> Q = new PriorityQueue<>(Comparator.comparingInt(n -> n.cost));
-	    Q.add(new Node(0, M[0][0], built, bestCost,9));
+	  public static int[] lccb2(int[][] M, int max) {
+		    // Inicializamos la variable upper.
+		    upper = Integer.MAX_VALUE;
 
-	    // Mientras la cola no esté vacía...
-	    while (!Q.isEmpty()) {
-	      // Extraemos el nodo con menor costo.
-	      Node n = Q.poll();
+		    // Inicializamos la variable bestIndex.
+		    bestIndex = -1;
+		    int centros [] = {0,0,0,0};
+		    int nivel = 0;
+		    
+		    // Inicializamos la cola de prioridad con el nodo raíz. Se define que el criterio para desencolar es el costo de U.
+		    PriorityQueue<Node> Q = new PriorityQueue<>(Comparator.comparingInt(n -> n.U));
 
-	      // Si el costo del nodo es menor o igual al del mejor nodo encontrado...
-	      if (n.cost <= bestCost) {
-	        // ...lo exploramos.
-	        for (int i = 0; i < M.length; i++) {
-	          if (M[n.index][i] > 0 && built[i] == 0) {
-	            // Calculamos el indicador c.
-	            int c = bestCost - n.cost;
+		    //Inicializamos el nodo raiz.
+		    Q.add(new Node(0,nivel,centros,upper,calcularC(M,centros)));
+		    // Inicializamos el array de resultados.
+		    int[] results = new int[M.length];
 
-	            // Si el indicador c es menor o igual a cero, descartamos la rama.
-	            if (c <= 0) {
-	              continue;
-	            }
+		    // Mientras la cola no esté vacía...
+		    while (!Q.isEmpty() && bestIndex < max) {
+		      // Extraemos el nodo con menor costo U.
+		      Node n = Q.poll();
 
-	            // Generamos los hijos del nodo.
-	            int[] newBuilt = built.clone();
-	            newBuilt[i] = 1;
-//	            Q.add(new Node(n.index + 1, n.cost + M[n.index][i], newBuilt));
-//	            Q.add(new Node(n.index + 1, n.cost, newBuilt));
-	          }
-	        }
-	      }
-	    }
+		      // Si el costo del nodo es menor o igual al del mejor nodo encontrado...
+		      if (n.U <= upper) {
+		    	  
+		    	  upper = n.U; //Actualizamos la referencia de upper
+		    	  //Calculamos RedMin y RedMax
+		    	  int redMin = calRedMin(M,n.centros,n.nivel);
+		    	  int redMax = calRedMax(M,n.centros,n.nivel);
+		    	  int costMantenimiento = M[n.nivel][0];
+		    	  
+		    	  if(costMantenimiento<redMin) {
+		    		  //El costo de mantenimiento es menor que red min
+		    		  centros[nivel]=1;
+		    		  nivel= nivel +1;
+		    		  Q.add(new Node(n.index +1, nivel,centros,calcularU(M,centros),calcularC(M,centros)));
 
-	    // Devolvemos el índice del mejor nodo encontrado.
-	    return bestIndex;
-	  }
+		    	  }
+		    	  else if(redMin<costMantenimiento && costMantenimiento<redMax) {
+		    		  //No se puede podar nada, tengo que seguir analizando, el costo de mantenimiento esta en el medio de las cotas
+		    		  
+		    		  
+		    		  int[] centrosCopia1= Arrays.copyOf(centros,centros.length);
+		    		  centrosCopia1[nivel]=1;
+		    		  Q.add(new Node(n.index +1, nivel+1,centrosCopia1,calcularU(M,centrosCopia1),calcularC(M,centrosCopia1)));
+		    		  
+		    		  int[] centrosCopia2 = Arrays.copyOf(centros,centros.length);
+		    		  centrosCopia2[nivel]=-1;
+		    		  Q.add(new Node(n.index +1, nivel+1,centrosCopia2,calcularU(M,centrosCopia2),calcularC(M,centrosCopia2)));
+
+		    		  nivel= nivel +1;
+		    	  }
+		    	  else {//Ya se que no lo voy a construir
+		    		  centros[nivel]=-1;
+		    		  nivel= nivel +1;
+		    		  Q.add(new Node(n.index +1, nivel,centros,calcularU(M,centros),calcularC(M,centros)));
+		    		  
+		    	  }
+
+
+		            // Actualizamos el resultado del nodo.
+//		            if (U <= n.C) {
+//		              results[n.index] = 1;
+//		            } else if (U < upper) {
+//		              results[n.index] = 0;
+//		            } else {
+//		              results[n.index] = -1;
+//		            }
+		          }
+		        }
+
+		    // Devolvemos el array de resultados.
+		    return results;
+		  }
 
 
 	}
